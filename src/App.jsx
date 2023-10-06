d6fc97b8b9801bdcc525604e77a9459f122b7a60
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-10-06 21:27:09 +0200

    feat: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Titre h1</h1>
+      <Outlet/>
     </>
   )
 }
