bdf0a4d71e20372c9eccdbc16def6472b23da266
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-10-13 22:47:27 +0200

    feat: Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+//import styles from '../assets/styles/post.module.css'
+
+function Post({author}) {
+    return (
+        <div>
+            <h3>{author.name}</h3>
+        </div>
+    )
+}
+
+export default Post
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,16 +2,19 @@ import styles from './Home.module.css'
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Post from "../../components/Post"
+import Card from "../../components/Card"
 import Select from '../../components/Select'
 
 function Home() {
     const [posts,setPosts] = useState(null)
+    const [categories,setCategories] = useState(null)
+    const [authors,setAuthors] = useState(null)
+    const [isLoading, setIsLoading] = useState({posts: true, categories: true, authors:true})
+    
     const [data,setData] = useState({
         search: "",
         category: "all"
     })
-    const [categories,setCategories] = useState(null)
-    const [isLoading, setIsLoading] = useState({posts: true, categories: true})
 
     useEffect(()=>{
         if( data.category === "all") {
@@ -43,6 +46,18 @@ function Home() {
             })
     },[])
 
+    useEffect(()=>{ 
+        fetch("http://localhost:4000/api/authors")
+            .then((response) => response.json())
+            .then((response) => {
+                setAuthors(response)
+                setIsLoading((values) => ({
+                    ...values,
+                    authors: false
+                }))
+            })
+    },[])
+
     function onChange(e) {
         setData({
             ...data,
@@ -65,20 +80,31 @@ function Home() {
     return (
         <>
             <h2>Home Page</h2>
+            <div className={styles["container--card"]}>
+                {!isLoading.authors 
+                    && authors.map((author) => <Card key={author._id} author={author}/>) 
+                }
+            </div>
             <Link to="/create">Ajouter un post</Link>
-            <div>
-                <label className={styles["label-style"]}>Rechercher
-                    <input className={styles["input-style"]} name='search' type="text" onChange={onChange} value={data.search} onInput={handleSearch}/>
-                </label>
-                <label className={styles["label-style"]}> Catégorie
-                    <Select style={styles["input-style"]} string="category" onChange={onChange}>
+            <div className={styles["container--search"]}>
+                <input 
+                    className={styles["style--input"]} 
+                    name='search' type="text" 
+                    onChange={onChange} 
+                    value={data.search} 
+                    onInput={handleSearch}
+                    placeholder='Rechercher'
+                    autoComplete='off'/>
+                <label className={styles["style--label"]}> Catégorie
+                    <Select style={styles["style--input"]} string="category" onChange={onChange}>
                         <option value="all">Tout</option>
                         {!isLoading.categories && categories.map((category) => <option key={category._id} value={category._id}>{category.title}</option>)}
                     </Select>
                 </label>
             </div>
-            {!isLoading.posts && posts.map((post) => <Post key={post._id} post={post}/>)}
-
+            <div className={styles["container--post"]}>
+                {!isLoading.posts && posts.map((post) => <Post key={post._id} post={post}/>)}
+            </div>
         </>
     )
 }
