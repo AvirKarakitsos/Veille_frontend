d35ccdb10fec9ca9a10d1d100f96f3789cf81a8c
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-11-18 17:36:39 +0100

    refactor: Forms addPost

--- src/pages/AddPost/AddPost.jsx
+++ src/pages/AddPost/AddPost.jsx
@@ -1,9 +1,4 @@
-import InputText from '../../components/InputText'
-import Select from '../../components/Select'
-import styles from './Form.module.css'
-import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useFetch } from '../../utils/useFetch'
 import styled from 'styled-components'
 
 const FormContainer = styled.div`
@@ -12,113 +7,17 @@ const FormContainer = styled.div`
     justify-content: center
 `
 
-function AddPost() {
-    const [data, setData] = useState({
-        title: "",
-        source: "",
-        link: "",
-        categoryId: "",
-        authorId: ""
-    })
-    const { table:categories, load: isLoadingCategories } = useFetch("http://localhost:4000/api/categories")
-    const { table:authors, load: isLoadingAuthors } = useFetch("http://localhost:4000/api/authors")
+function AddElement() {
     
-    const formAdd = useRef()
-    const formMessage = useRef()
-
-    function onChange(e) {
-        setData({
-            ...data,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleAdd = function(e) {
-        e.preventDefault()
-        if((data.title === "") || (data.categoryId === "") || (data.authorId === "")) {
-            formMessage.current.innerHTML = "Veuillez compléter tous les champs"
-        } else {
-            fetch("http://localhost:4000/api/posts",{
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            })
-                .then(response => {
-                    if(response.ok) {
-                        setData( values => ( {
-                            ...values,
-                            title: '',
-                            link: '',
-                            source: '',
-                            categoryId: '',
-                            authorId: ''
-                        } ))
-                        formAdd.current.reset()
-                    } 
-                    return response.json()
-                })
-                .then(response => {
-                    formMessage.current.innerHTML = ""
-                    console.log(response.message)
-                })
-                .catch(err => console.log(err.message))
-        }
-    }
-
     return(
         <>
             <h2>Formulaire</h2>
             <FormContainer>
-            <form ref={formAdd} className={styles["form-container"]} onSubmit={handleAdd}>
-                <label className={styles["label-style"]}>Titre
-                    <InputText style={styles["input-style"]} string="title" value={data.title} onChange={onChange}/>
-                </label>
-
-                <label className={styles["label-style"]}>Source
-                    <Select style={styles["input-style"]} string="source" onChange={onChange}>
-                        <option value=""></option>
-                        <option value="twitter">Twitter</option>
-                        <option value="youtube">Youtube</option>
-                        <option value="article">Article</option>
-                    </Select>
-                </label>
-
-                <label className={styles["label-style"]}>Lien
-                    <InputText style={styles["input-style"]} string="link" value={data.link} onChange={onChange}/>
-                </label>
-
-                <label className={styles["label-style"]}>Catégorie
-                    {!isLoadingCategories && <Select style={styles["input-style"]} string="categoryId" onChange={onChange}>
-                        <option value=""></option>
-                        {categories.map((category) => {
-                        
-                            return (<option key={category._id} value={category._id}>{category.title}</option>)
-                            
-                        })}
-                    </Select>}
-                </label>
-
-                <label className={styles["label-style"]}>Auteur
-                    {!isLoadingAuthors && <Select style={styles["input-style"]} string="authorId" onChange={onChange}>
-                        <option value=""></option>
-                        {authors.map((author) => {
-
-                            return (<option key={author._id} value={author._id}>{author.name}</option>)
-
-                        })}
-                    </Select>}
-                </label>
-
-                <p ref={formMessage}></p>
-
-                <button type="submit">Valider</button>
-            </form>
+                
             </FormContainer>
             <Link to="/">{"Retour à la page d'accueil"}</Link>
         </>
     )
 }
 
-export default AddPost
+export default AddElement
