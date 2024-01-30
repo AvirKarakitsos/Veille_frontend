f85d534e7578687ad31ff54c9d5fd60a12dc5515
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-01-30 11:31:36 +0100

    refactor: display arrows icones

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,28 +1,37 @@
 import styles from "../assets/styles/aside.module.css"
 import Card from "./Card"
 import { useFetch } from "../utils/useFetch"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useViewport } from "../utils/useViewport"
+import styled from "styled-components"
 
 
+const IconeDown = styled.i`
+    display: ${(props) => props.isDisplay? "block" : "none"};
+`
+const IconeRight = styled.i`
+    display: ${(props) => props.isDisplay? "block" : "none"};
+`
 function Aside() {
     const { table: authors, load: isLoadingAuthors } = useFetch("http://localhost:4000/api/authors")
     const refContainer = useRef(null)
-    const refIcone = useRef(null)
-    const refIcone2 = useRef(null)
+    const [isDisplay, setIsDisplay] = useState({iconeDown: true, iconeRight: false})
     const windowWidth = useViewport()
     
     useEffect(()=> {
+        if(windowWidth > 768) {
+            setIsDisplay((values) => ({...values, iconeDown: true, iconeRight: false}))
+        } else setIsDisplay((values) => ({...values, iconeDown: false, iconeRight: true}))
+        
         refContainer.current.addEventListener("scroll",()=>{
-          
             if(windowWidth > 768) {
                 if(refContainer.current.scrollTop + refContainer.current.offsetHeight >= refContainer.current.scrollHeight) {
-                    refIcone.current?.classList.add("hidden")
-                } else refIcone.current.classList.remove("hidden")
+                    setIsDisplay((values) => ({...values, iconeDown: false, iconeRight: false}))
+                } else setIsDisplay((values) => ({...values, iconeDown: true, iconeRight: false}))
             } else {
                 if(refContainer.current.scrollLeft + refContainer.current.offsetWidth >= refContainer.current.scrollWidth) {
-                    refIcone2.current?.classList.add("hidden")
-                } else refIcone2.current.classList.remove("hidden")
+                    setIsDisplay((values) => ({...values, iconeRight: false, iconeDown:false}))
+                } else setIsDisplay((values) => ({...values, iconeRight: true, iconeDown:false}))
             }
            
         })
@@ -34,15 +43,11 @@ function Aside() {
                 {!isLoadingAuthors 
                     && authors.map((author) => <Card key={author._id} author={author}/>) 
                 }
-                {windowWidth > 768
                 
-                ? <div  className={styles.icone}>
-                    <i ref={refIcone} className="fa-solid fa-circle-arrow-down"></i>    
+                <div  className={styles.icone}>
+                    <IconeDown className="fa-solid fa-circle-arrow-down" isDisplay={isDisplay.iconeDown}></IconeDown>    
+                    <IconeRight className="fa-solid fa-circle-arrow-right" isDisplay={isDisplay.iconeRight}></IconeRight> 
                 </div>
-                : <div  className={styles.icone}>
-                        <i ref={refIcone2} className="fa-solid fa-circle-arrow-right"></i>    
-                    </div>
-                }
             </div>
         </aside>
     )
